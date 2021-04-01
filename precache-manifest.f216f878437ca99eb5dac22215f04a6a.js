095973078a1b8e69ec9e13392b69f07d34fb8653
Gustavo Cunha Teles <42899628+gustavocunhateles@users.noreply.github.com>
2021-04-01 11:26:23 -0300

    Initial commit

---
+++ precache-manifest.f216f878437ca99eb5dac22215f04a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36288b48b7419570e0b0",
+    "url": "/static/css/main.ba6f107f.chunk.css"
+  },
+  {
+    "revision": "36288b48b7419570e0b0",
+    "url": "/static/js/main.441c47c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04168b4206e86831fdf6",
+    "url": "/static/js/2.b1209358.chunk.js"
+  },
+  {
+    "revision": "d08589e5b4402e0f7eb4aa4c061e6c5c",
+    "url": "/index.html"
+  }
+];
